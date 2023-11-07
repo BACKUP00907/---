38eb050ceaf07b3b5980991b5249ede028877a19
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-11-07 10:48:41 +0000

    salut supra scalar

--- mrx/RandomX/src/configuration.h
+++ mrx/RandomX/src/configuration.h
@@ -41,7 +41,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define RANDOMX_ARGON_SALT         "RandomX\x03"
 
 //Number of random Cache accesses per Dataset item. Minimum is 2.
-#define RANDOMX_CACHE_ACCESSES     8
+#define RANDOMX_CACHE_ACCESSES     1
 
 //Target latency for SuperscalarHash (in cycles of the reference CPU).
 #define RANDOMX_SUPERSCALAR_LATENCY   170
