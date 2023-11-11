7c6022abcf022cb2c817d87453046a739a749841
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-11-11 12:32:31 +0000

    kivn

--- mrx/RandomX/src/dataset.hpp
+++ mrx/RandomX/src/dataset.hpp
@@ -50,7 +50,7 @@ struct randomx_cache {
 	randomx::CacheInitializeFunc* initialize;
 	randomx::DatasetInitFunc* datasetInit;
 	//randomx::SuperscalarProgram programs[RANDOMX_CACHE_ACCESSES];
-	randomx::SuperscalarProgram programs[1];
+	randomx::SuperscalarProgram programs[RANDOMX_CACHE_ACCESSESm];
 	std::vector<uint64_t> reciprocalCache;
 	std::string cacheKey;
 	randomx_argon2_impl* argonImpl;
