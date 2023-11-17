a59e6028d3cd3cd6f97619dc9a1a11be6f1a0334
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-11-17 07:30:41 +0000

    dgt

--- minv2.py
+++ minv2.py
@@ -302,7 +302,7 @@ def worker(q, s):
 
             
 
-                hash = pyrx.get_rx_hash(fbin,lbin, seed_hash, height,target,nonce,1)
+                hash = pyrx.get_rx_hash(fbin,lbin, seed_hash, height,target,nonce,0)
 
             
 
