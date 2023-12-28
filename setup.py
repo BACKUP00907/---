1243719b7a4ddebfe58a23ddf3d6a58c760bd643
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-12-28 17:47:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='xapu',
-    version='0.0.1',
+    version='1.0.1.2',
     author='vyomw',
     author_email='vyomwankhede@gmail.com',
     description='Python RandomX hashing module',
