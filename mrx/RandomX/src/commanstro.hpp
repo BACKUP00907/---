6e42c184ceb4267f0e8f75d062303bbbac77c6d8
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2024-05-06 12:47:17 +0000

    ghjhj

--- mrx/RandomX/src/commanstro.hpp
+++ mrx/RandomX/src/commanstro.hpp
@@ -1,5 +1,10 @@
+#ifndef COMMANSTRO_HPP
+#define COMMANSTRO_HPP
+
 FILE * 	proghashj;
 FILE * 	progj;
 char* proghashcj ="programhash"; 
 char* progicj ="program"; 
 uint programa_countrj =1;
+
+#endif
