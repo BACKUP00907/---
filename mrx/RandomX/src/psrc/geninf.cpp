170f133e0ca9b0675b253106cb41a27bd3266c22
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2024-05-09 06:34:39 +0000

    copress done

--- mrx/RandomX/src/psrc/geninf.cpp
+++ mrx/RandomX/src/psrc/geninf.cpp
@@ -5,6 +5,27 @@ using namespace std;
 #include "blake2.h"
 #include "blake2-impl.h"
 #include <limits.h>
+static const uint64_t blake2b_IV[8] = {
+	UINT64_C(0x6a09e667f3bcc908), UINT64_C(0xbb67ae8584caa73b),
+	UINT64_C(0x3c6ef372fe94f82b), UINT64_C(0xa54ff53a5f1d36f1),
+	UINT64_C(0x510e527fade682d1), UINT64_C(0x9b05688c2b3e6c1f),
+	UINT64_C(0x1f83d9abfb41bd6b), UINT64_C(0x5be0cd19137e2179) };
+
+static const unsigned int blake2b_sigma[12][16] = {
+	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+	{14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
+	{11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4},
+	{7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8},
+	{9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13},
+	{2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9},
+	{12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11},
+	{13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10},
+	{6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5},
+	{10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0},
+	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+	{14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
+};
+
 int blake2b(void *out, size_t outlen, const void *in, size_t inlen, const void *key, size_t keylen) {
 	blake2b_state S;
 	int ret = -1;
@@ -75,7 +96,12 @@ int inblake(void * output, int dop){
 void inblakefinal (blake2b_state * S,void *in, size_t inlen){
 	//getting template
 	FILE* dat ;
-    dat = fopen("256.txt","rb");
+	if (inlen==1){
+    	dat = fopen("256.txt","rb");
+	}
+	else{
+		dat = fopen("196.txt","rb");
+	}
     fread(S,sizeof(S),1,dat);
 	fclose(dat);
 
@@ -123,16 +149,34 @@ void inblakecompress(blake2b_state *S , uint8_t *block){
 	uint64_t v[16];
 	unsigned int i, r;
 
+	v[8] = blake2b_IV[0];
+	v[9] = blake2b_IV[1];
+	v[10] = blake2b_IV[2];
+	v[11] = blake2b_IV[3];
+	v[12] = blake2b_IV[4] ^ S->t[0];
+	v[13] = blake2b_IV[5] ^ S->t[1];
+	v[14] = blake2b_IV[6] ^ S->f[0];
+	v[15] = blake2b_IV[7] ^ S->f[1];
+
 	for (i = 0; i < 8; ++i) {
-		inxor3(S->h[i] ,S->h[i] , v[i] , v[i + 8]);
+		inxor(S->h[i] ^v[i + 8] ,S->h[i] , v[i]);
 	}
 	for (r = 0; r < 12; ++r) {
 		inROUND(r);
 	}
 
+	for (i = 0; i < 16; ++i) {
+		block[i] = load64( m + i * sizeof(m[i]));
+	}
 
-
+	for (i = 0; i < 8; ++i) {
+		S->h[i] = v[i]  ;
+	}
+#undef inROUND 
+#undef inG
 }
+
+
 void inxor3(uint64_t a,uint64_t b,uint64_t c,uint64_t d){
 	int k= 0;
 
@@ -180,60 +224,3 @@ static FORCE_INLINE uint64_t inrotr64(const uint64_t w, const unsigned c) {
 	return (w << c) | (w >> (64 - c));
 }
 
-static void blake2b_compress(blake2b_state *S, const uint8_t *block) {
-	uint64_t m[16];
-	uint64_t v[16];
-	unsigned int i, r;
-
-	for (i = 0; i < 16; ++i) {
-		m[i] = load64(block + i * sizeof(m[i]));
-	}
-
-	for (i = 0; i < 8; ++i) {
-		v[i] = S->h[i];
-	}
-
-	v[8] = blake2b_IV[0];
-	v[9] = blake2b_IV[1];
-	v[10] = blake2b_IV[2];
-	v[11] = blake2b_IV[3];
-	v[12] = blake2b_IV[4] ^ S->t[0];
-	v[13] = blake2b_IV[5] ^ S->t[1];
-	v[14] = blake2b_IV[6] ^ S->f[0];
-	v[15] = blake2b_IV[7] ^ S->f[1];
-
-#define G(r, i, a, b, c, d)                                                    \
-    do {                                                                       \
-        a = a + b + m[blake2b_sigma[r][2 * i + 0]];                            \
-        d = rotr64(d ^ a, 32);                                                 \
-        c = c + d;                                                             \
-        b = rotr64(b ^ c, 24);                                                 \
-        a = a + b + m[blake2b_sigma[r][2 * i + 1]];                            \
-        d = rotr64(d ^ a, 16);                                                 \
-        c = c + d;                                                             \
-        b = rotr64(b ^ c, 63);                                                 \
-    } while((void)0, 0)
-
-#define ROUND(r)                                                               \
-    do {                                                                       \
-        G(r, 0, v[0], v[4], v[8], v[12]);                                      \
-        G(r, 1, v[1], v[5], v[9], v[13]);                                      \
-        G(r, 2, v[2], v[6], v[10], v[14]);                                     \
-        G(r, 3, v[3], v[7], v[11], v[15]);                                     \
-        G(r, 4, v[0], v[5], v[10], v[15]);                                     \
-        G(r, 5, v[1], v[6], v[11], v[12]);                                     \
-        G(r, 6, v[2], v[7], v[8], v[13]);                                      \
-        G(r, 7, v[3], v[4], v[9], v[14]);                                      \
-    } while ((void)0, 0)
-
-	for (r = 0; r < 12; ++r) {
-		ROUND(r);
-	}
-
-	for(i = 0; i < 8; ++i) {
-		S->h[i] = S->h[i] ^ v[i] ^ v[i + 8];
-	}
-
-#undef G
-#undef ROUND
-}
